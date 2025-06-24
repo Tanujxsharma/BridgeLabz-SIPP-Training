0db13258105775ff996265f83d2b45bb49e4aa15
Tanujxsharma <ta904513@gmailcom>
2025-06-24 10:04:29 +0530

    Initial empty commit

--- PracticeProblems/Perameter_Rect.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Perameter_Rect { 
-   public static void main(String[] args) {   
-     Scanner sc = new Scanner(System.in);       
-      float l = sc.nextFloat();     
-      float b = sc.nextFloat();  
-      System.out.println(l*b);  
-  }
-}
