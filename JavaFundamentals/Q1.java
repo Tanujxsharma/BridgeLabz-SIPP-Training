c08bef3e15711596c7bfa8de7755202515c03729
Tanuj Sharma <122309916+Tanujxsharma@users.noreply.github.com>
2025-06-24 09:37:02 +0530

    Delete JavaFundamentals directory

--- JavaFundamentals/Q1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Q1{
-    public static void main(String[] args) {
-       Scanner sc =new Scanner(System.in);
-       double KM=sc.nextDouble();
-       System.out.print("KM to Miles :");
-       System.out.println(KM*1.6); 
-    }
-}
--- JavaFundamentals/Q2.java
+++
@@ -1,5 +0,0 @@
-public class Q2 {
- public static void main(String[] args) {
-    System.out.println(10.8*1.6);
- }   
-}
